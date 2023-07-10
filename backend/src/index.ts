b74090ada098f0c2be447df8563f3a19395360a4
Jae <jaeladu1@gmail.com>
2023-07-10 13:06:43 -0300

    Express server initial config

---
+++ backend/src/index.ts
@@ -0,0 +1,15 @@
+import express, { json } from "express";
+
+const app = express();
+const port = process.env.PORT || 3000;
+const environment = process.env.ENVIRONMENT;
+
+app.use(json());
+
+app.get("/up", (req, res) => {
+   res.send(`Server up and running in ${environment} mode bitch`);
+});
+
+app.listen(port, () => {
+   console.log(`Server up and running in ${environment} mode in port ${port}`);
+});
